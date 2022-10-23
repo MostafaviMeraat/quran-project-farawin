8b6b999a6e9267335f1d11f59f3f15bdc33203d7
Meraat <Meraat@gmail.com>
2022-10-23 20:39:30 +0330

    initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
+import { type } from '@testing-library/user-event/dist/type';
 import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
+import { suras, pages } from './quran-resources (farawin)/quran-metadata'
+import { emla } from './quran-resources (farawin)/quran-text-emla'
+
 
 function App() {
+  //ts
+
+  // variables
+  let tempArr: any[] = []
+  const [newSuras, setNewSuras] = useState([])
+
+  // useEffects 
+
+  useEffect(() => { allSuras(suras) }, [])
+
+  // functions
+
+  const allSuras = (suras: any[]) => {
+    for (let c = 0; c < suras.length - 1; c++) {
+      if (c === 0 || c === 8) {
+        let arr = []
+        for (let c1 = suras[c][0]; c1 < suras[c][0] + suras[c][1]; c1++) {
+          arr.push(emla[c1])
+        }
+        tempArr.push(arr)
+      }
+      else {
+        let arr = []
+        arr.push("بِسۡمِ اللّٰهِ الرَّحۡمٰنِ الرَّحٖیمِ")
+        for (let c1 = suras[c][0]; c1 < suras[c][0] + suras[c][1]; c1++) {
+          arr.push(emla[c1])
+        }
+        tempArr.push(arr)
+      }
+    }
+    console.log(tempArr);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
-
 export default App;
