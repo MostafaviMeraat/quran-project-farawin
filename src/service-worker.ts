8e4c7e462c42d99d8d006ef9ff028fdeffd76870
Meraat <Meraat@gmail.com>
2022-11-08 19:25:34 +0330

    first try to make it PWA but failed

---
+++ src/service-worker.ts
@@ -0,0 +1,8 @@
+/// <reference lib="webworker" />
+/// <reference no-default-lib="true" />
+
+import { precacheAndRoute } from 'workbox-precaching'
+
+declare const self: ServiceWorkerGlobalScope
+
+precacheAndRoute(self.__WB_MANIFEST)
